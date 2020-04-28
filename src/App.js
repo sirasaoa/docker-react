4cc3111ad80b07baf5ff9d01be5a25d2a71cc20d
sirasaoa <35425430+sirasaoa@users.noreply.github.com>
2020-04-28 20:37:06 +0530

    Change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed on the feature branch!
+          Hey! I am hosted on EBS.
         </p>
         <a
           className="App-link"
