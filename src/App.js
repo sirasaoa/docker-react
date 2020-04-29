7b0e99eac821a3c2a81b59d02befc221fe5b2d9e
sirasaoa <35425430+sirasaoa@users.noreply.github.com>
2020-04-29 10:11:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey! I am hosted on EBS.
+          Hey!I am hosted on AWS.
         </p>
         <a
           className="App-link"
