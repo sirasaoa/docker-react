87c77f160ae3d8ded3b1020970642f0e3d24d1e5
sirasaoa <35425430+sirasaoa@users.noreply.github.com>
2020-04-29 11:05:42 +0530

    Change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey!I am hosted on AWS.
+          Hey! This is a demo page.
         </p>
         <a
           className="App-link"
